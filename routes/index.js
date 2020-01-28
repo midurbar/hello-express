6e0f11ad3f019a6da9b533ab3456064ceb18a336
afd <afd@alup5.academiapostal.es.local>
2020-01-28 20:56:58 +0100

    Camb

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const products=[
+    {name: "Microondas", price: 45, stock: 6},
+    {name: "Frigorifico", price: 200, stock:4},
+    {name: "Lampara", price: 20, stock:10},
+    {name: "TV", price: 90, stock:0},
+    {name: "Lavadora", price: 290, stock:3},
+    {name: "Secadora", price: 180, stock:5}
+  ];
+  res.render('index', { title: 'Mike Express', products });
 });
 
 module.exports = router;
