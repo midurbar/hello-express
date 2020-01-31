0d50e05eb7be3edf19641f0e5f23be7f8e1f8630
afd <afd@alup5.academiapostal.es.local>
2020-01-31 20:53:25 +0100

    camb login

---
+++ models/users.js
@@ -0,0 +1,4 @@
+module.exports=[
+    {username:"mike", password:"123456"},
+    {username:"admin", password:"654321"}
+];
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var products=require('../models/products.js');
+var user=require('../models/users.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Mike Express', products });
+  const username=req.session.username;
+  res.render('index', { title: 'Mike Express', username, products });
 });
 
 router.get('/products/:ref', function(req, res, next) {
@@ -21,5 +23,41 @@ router.get('/products/:ref', function(req, res, next) {
 
 });
 
+var cesta= [];
+
+router.post("/comprar", function (req, res, next){
+  const ref=req.body.ref;
+
+  const product=products.find(function(p) {
+    return p.ref==ref;
+  });
+
+  cesta.push(product);
+
+  res.redirect("/");
+});
+
+router.get("/login", function (req, res, next){
+  res.render('login');
+});
+
+/*Procesa el formulario del ogin. Recibe los datos del formulario en la peticion (req) y comprueba si existe
+algun usuario con ese nombre y contraseña. De ser asi, crea una cookie y lo manda a la pagina principal.
+De no coincidir, carga de nuevo la pagina del login y muestra un error,*/
+router.post("/login",function (req, res, next) {
+  const username=req.body.username;
+  const password=req.body.password;
+  const users=user.find(function (u) {
+    return (u.username==username && u.password==password);
+  });
+
+  if (users){
+    req.session.username = username;
+    res.redirect("/");
+  }else {
+    res.render("login");
+  }
+
+});
 
 module.exports = router;
